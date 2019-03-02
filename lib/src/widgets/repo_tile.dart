dc8105700a3a71829a742b300750e79f12466292
Alberto Méndez Hernández <albertomendezhdz@gmail.com>
2019-03-02 13:30:54 +0100

    Open repos in browser

--- lib/src/widgets/repo_tile.dart
+++ lib/src/widgets/repo_tile.dart
@@ -1,5 +1,6 @@
 import 'package:alpha_flutter_workshop/src/models/repo.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class RepoTile extends StatelessWidget {
   final Repo repo;
@@ -13,9 +14,23 @@ class RepoTile extends StatelessWidget {
         ListTile(
           title: Text(repo.name),
           subtitle: Text(repo.description),
+          trailing: RaisedButton(
+            child: Text('Open'),
+            color: Colors.lime,
+            textColor: Colors.green,
+            onPressed: _launchURL,
+          ),
         ),
         Divider()
       ],
     );
+  }
+
+  _launchURL() async {
+    if (await canLaunch(repo.htmlUrl)) {
+      await launch(repo.htmlUrl);
+    } else {
+      throw 'Could not launch ${repo.htmlUrl}';
+    }
   }
 }
